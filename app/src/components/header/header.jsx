a72eeba6a12331e06f82fbb15962fa9454784f53
austinhatch <ahatch@colgate.edu>
2024-09-25 15:40:37 -0400

    header reformat

--- app/src/components/header/header.jsx
+++ app/src/components/header/header.jsx
@@ -1,32 +1,32 @@
 import React, { useState } from "react";
 import styles from "./header.module.scss";
+import namelessLogo from "../../assets/nameless-logo-dark.png";
 
 const routes = [
-    {
-        id: "Create",
-        url: "/",
-    },
-    {
-        id: "My Geos",
-        url: "/my-geos",
-    }
-]
+  {
+    id: "Create",
+    url: "/",
+  },
+  {
+    id: "My Geos",
+    url: "/my-geos",
+  },
+];
 
 const Header = () => {
-
-    return (
-        <div className={styles.parentContainer}>
-            <h1>GeoFencing</h1>
-            <div className={styles.routeContainer}>
-                {routes.map((route) => (
-                    <a key={route.id} href={route.url}>
-                        {route.id}
-                    </a>
-                ))}
-            </div>
-        </div>
-    );
-
-}
+  return (
+    <div className={styles.parentContainer}>
+      <img src={namelessLogo} className={styles.logo} alt="Nameless Logo" />
+      <h1>GeoFencing</h1>
+      <div className={styles.routeContainer}>
+        {routes.map((route) => (
+          <a key={route.id} href={route.url}>
+            {route.id}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
