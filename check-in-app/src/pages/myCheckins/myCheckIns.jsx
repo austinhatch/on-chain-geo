d4180341918952514bd56419249c0b23b77bad2c
austinhatch <ahatch@colgate.edu>
2024-10-14 16:16:06 -0400

    start checkin card

--- check-in-app/src/pages/myCheckins/myCheckIns.jsx
+++ check-in-app/src/pages/myCheckins/myCheckIns.jsx
@@ -1,7 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./myCheckIns.module.scss";
+import { getOwnedCheckins } from "../../utils/aptos/geoUtils";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
 
 const MyCheckIns = () => {
+  const [checkIns, setCheckIns] = useState([]);
+  const { wallet, account, signAndSubmitTransaction } = useWallet();
+
+  useEffect(() => {
+    // fetch check ins
+    const getCheckIns = async () => {
+      const checkIns = await getOwnedCheckins(account.address);
+      setCheckIns(checkIns);
+    }
+    getCheckIns();
+
+  }, []);
   return (
     <div className={styles.parentContainer}>
       <div className={styles.headerContainer}>
