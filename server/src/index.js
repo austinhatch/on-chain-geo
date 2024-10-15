84ed9264b636b22126f175ebddc876a92a8d63ea
austinhatch <ahatch@colgate.edu>
2024-10-15 12:02:38 -0400

    fix api

--- server/src/index.js
+++ server/src/index.js
@@ -1,31 +1,32 @@
 import express from "express";
-import { aptos } from "./configs/aptos.js";
+import { aptos, getAptosAccount } from "./configs/aptos.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3002; // Your server will run on localhost:3001
+const port = process.env.PORT || 3002; // Your server will run on localhost:3002
 
 app.use(express.json());
 
-// Handle POST request
 app.post("/api/verify-location", async (req, res) => {
+  console.log(req.body);
   const { geo_address, lat, lat_is_neg, lng, lng_is_neg, user_address } =
     req.body;
 
   try {
     // Create the transaction payload
-    const func = `${process.env.REACT_APP_GEO_CONTRACT_ADDRESS}::on_chain_geo::is_within_geo`;
+    const func = `${process.env.GEO_CONTRACT_ADDRESS}::on_chain_geo_v1::is_within_geo`;
 
     // Get the account from the config
-    const account = aptos.config.account;
+    const aptosAccount = getAptosAccount();
+    console.log(aptosAccount);
 
     //Generate a random token_id
     const token_id = Math.floor(10000000 + Math.random() * 90000000).toString();
 
     const transaction = await aptos.transaction.build.simple({
-      sender: account.accountAddress,
+      sender: aptosAccount.accountAddress,
       data: {
         function: func,
         typeArguments: [],
@@ -43,21 +44,21 @@ app.post("/api/verify-location", async (req, res) => {
 
     // submit transaction
     const committedTransaction = await aptos.signAndSubmitTransaction({
-      signer: account,
+      signer: aptosAccount,
       transaction: transaction,
     });
+    console.log(committedTransaction);
+
     const waitForTransaction = await aptos.waitForTransaction({
       transactionHash: committedTransaction.hash,
     });
 
     if (waitForTransaction.success) {
-      return {
-        status: "success",
-        message: "Token Created Successfully",
-      };
+      res.send({ status: "success", message: "Token Created Successfully" });
     }
-  } catch {
-    return { status: "error", message: "Error Minting Token" };
+  } catch (e) {
+    console.log(e);
+    res.send({ status: "error", message: `Error Creating Token: ${e}` });
   }
 });
 
